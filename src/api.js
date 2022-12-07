45beab0e943f1b8b7d63af7fc02aeaabe8792f1d
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-12-07 09:00:40 +0000

    this will add the getsinglereview function to return data by review id

--- src/api.js
+++ src/api.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 
+const gameApi = axios.create({
+  baseURL: 'https://eager-getup-ant.cyclic.app/api/'
+})
+
+
 export const getReviews = () => {
   return axios
     .get("https://eager-getup-ant.cyclic.app/api/reviews")
@@ -7,3 +12,10 @@ export const getReviews = () => {
       return reviews;
     });
 };
+
+export const getSingleReview = ( review_id ) => {
+  return gameApi.get(`reviews/${review_id}`).then((res) => {
+      console.log(res.data)
+      return res.data.review
+    })
+  }
