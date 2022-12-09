ac9bc76cbd5fa1239f8f28a85ff1b406865f09ac
RyanNolan1 <114003965+RyanNolan1@users.noreply.github.com>
2022-12-09 10:51:30 +0000

    this will add the functions to upvote and downvote the review

--- src/api.js
+++ src/api.js
@@ -18,10 +18,22 @@ export const getSingleReview = (review_id) => {
   });
 };
 
-// added a function to return comments
-
 export const getReviewComments = (review_id) => {
   return gameApi.get(`reviews/${review_id}/comments`).then((res) => {
     return res.data.review;
   });
+};
+
+export const upVoteReview = (review_id) => {
+  return gameApi.patch(`reviews/${review_id}`, {inc_votes: 1})
+  .then((res) => {
+    return res.data.votes
+  })
+};
+
+export const downVoteReview = (review_id) => {
+  return gameApi.patch(`reviews/${review_id}`, {inc_votes: -1})
+  .then((res) => {
+    return res
+  })
 };
